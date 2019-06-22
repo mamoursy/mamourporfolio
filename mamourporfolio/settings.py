c519f95851b15fac2178dd61a759f692e793ae60
mamoursy <mamoursy@gmail.com>
2019-06-23 00:08:15 +0100

    final add

--- mamourporfolio/settings.py
+++ mamourporfolio/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,4 +120,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+
+    os.path.join(BASE_DIR, 'jobs/static')
+
+]
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+
+
+try:
+
+    from .local_settings import *
+
+except ImportError:
+
+    pass
